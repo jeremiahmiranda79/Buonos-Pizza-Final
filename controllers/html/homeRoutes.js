3554b6d32794781e7a9e4b24c96780b8dcb52249
Jeremiah Miranda <jeremiahmiranda79@gmail.com>
2023-10-15 14:06:35 -0700

    added menu cards

--- controllers/html/homeRoutes.js
+++ controllers/html/homeRoutes.js
@@ -2,38 +2,54 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Categories } = require('../../models');
 
-
 // Homepage
 router.get('/', async (req, res) => {
-    res.render('homepage', {
-        // loggedIn: req.session.loggedIn, 
-        // name: req.session.name
-    });
+	res.render('homepage', {
+		// loggedIn: req.session.loggedIn, 
+		// name: req.session.name
+	});
 });
 
 router.get('/contact', async (req, res) => {
-    res.render('contact', {
-        // loggedIn: req.session.loggedIn,
-        // name: req.session.name
-    });
+	res.render('contact', {
+		// loggedIn: req.session.loggedIn,
+		// name: req.session.name
+	});
 });
 
 router.get('/menu', async (req, res) => {
-    res.render('menu', {
+	res.render('menu', {
 
-    })
-})
+	});
+});
 
 router.get('/yelp', async (req, res) => {
-    res.render('yelp', {
-        
-    })
-})
+	res.render('yelp', {
+			
+	});
+});
 
 router.get('/about', async (req, res) => {
-    res.render('about', {
-        
-    })
-})
+	res.render('about', {
+			
+	});
+});
+
+router.get('/team', async (req, res) => {
+	res.render('team', {
+
+	});
+});
+
+router.get('/score', async (req, res) => {
+	res.render('score', {
+
+	});
+});
+
+router.get('/login', async (req, res) => {
+	// if (req.session.loggedIn) return res.redirect('../');
+			res.status(200).render('login');
+});
 
 module.exports = router;
